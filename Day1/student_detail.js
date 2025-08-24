49976285eed93f29c94a1dfc491ff21efd1a74c3
Pawani0 <rishabhpawani09@gmail.com>
2025-08-24 23:34:36 +0530

    Day 1 task

---
+++ Day1/student_detail.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Rishabh Pawani",
+  age: 20,
+  rollNo: "0187AS231044",
+  branch: "AI-DS"
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Roll No:", student.rollNo);
+console.log("Branch:", student.branch);
